f260f5ea7449833592be3be6cd5c430640af7807
Lemuel Figueira <pereiraleleo87@gmail.com>
2022-02-04 17:05:59 -0300

    feat: :tada:

---
+++ src/routes/index.svelte
@@ -0,0 +1,8 @@
+<h1>Hello</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<button class="global-button"> OK </button>
+
+<style type="text/scss">
+  @import '../styles/global.scss';
+</style>

760e60bae6bcf4be76a7b4b737d688a8e6d1f600
Lemuel Figueira <pereiraleleo87@gmail.com>
2022-02-06 15:01:04 -0300

    feat: new cors

--- src/services/rastreio/index.ts
+++ src/services/rastreio/index.ts
@@ -4,7 +4,7 @@ import { RastreioResponse } from './index.types';
 export async function buscarObjeto(codigo: string): Promise<RastreioResponse> {
   const config: AxiosRequestConfig = {
     method: 'GET',
-    url: `https://thingproxy.freeboard.io/fetch/https://proxyapp.correios.com.br/v1/sro-rastro/${codigo}`,
+    url: `https://cors-all-proxy.herokuapp.com/fetch/https://proxyapp.correios.com.br/v1/sro-rastro/${codigo}`,
   };
 
   const { data } = await axios.request(config);

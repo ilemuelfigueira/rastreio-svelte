d5de923d539d5d7a7ac19037713f766e778c315b
Lemuel Figueira <pereiraleleo87@gmail.com>
2022-02-25 00:57:49 -0300

    fix: loading

--- src/components/loading.svelte
+++ src/components/loading.svelte
@@ -4,11 +4,13 @@
   div {
     min-width: 2rem;
     aspect-ratio: 1/1;
-    background: linear-gradient(45deg, transparent, transparent 45%, var(--secondary));
     border-radius: 50%;
 
     position: relative;
     animation: animate 2s linear infinite;
+
+    border: 5px solid var(--primary);
+    border-top-color: var(--text);
   }
 
   @keyframes animate {
@@ -18,29 +20,5 @@
     100% {
       transform: rotate(360deg);
     }
-  }
-
-  div:before {
-    content: '';
-    position: absolute;
-    top: 0.2rem;
-    left: 0.2rem;
-    right: 0.2rem;
-    bottom: 0.2rem;
-    background: var(--primary);
-    border-radius: 50%;
-    z-index: 1000;
-  }
-  div:after {
-    content: '';
-    position: absolute;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    background: linear-gradient(45deg, transparent, transparent 45%, var(--secondary));
-    border-radius: 50%;
-    z-index: 1;
-    filter: blur(0.5rem);
   }
 </style>

7ad133f1fe37a9dcfc23a338794f421fb650b57c
LemuelFigueira <pereiraleleo87@gmail.com>
2022-03-01 15:19:52 -0300

    feat: introduction page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,1 +1,6 @@
-<h1>Rastreio</h1>
+<script>
+  import Button from '../components/button.svelte';
+</script>
+
+<h1>Bem vindo ao Rastreio-Svelte.</h1>
+<Button on:click={() => window.location.assign('tracking')}>Ir para aplicação</Button>
